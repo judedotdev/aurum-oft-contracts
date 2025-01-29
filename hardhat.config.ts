c0e280064574ba08b67f482a82e1bec64df78f37
Jude <silentcodear@gmail.com>
2025-01-29 14:04:01 +0100

    verify OFT contract on re.al

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -112,7 +112,7 @@ const config: HardhatUserConfig = {
         apiKey: {
             "sonic-testnet": process.env.SONICSCAN_API_KEY || "",
             "amoy-testnet": process.env.POLYGONSCAN_API_KEY || "",
-            "real": process.env.REALSCAN_API_KEY || "",
+            "real": "empty",
         },
         customChains: [
             {
